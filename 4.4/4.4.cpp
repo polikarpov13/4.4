df6dd855f4f1b7d55a3304ce752a0997e29e9ccd
polikarpov13 <vladyslav.polikarpov.ik.2021@lpnu.ua>
2021-10-02 20:27:29 +0300

    first master commit

---
+++ 4.4/4.4.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+    double R, xp, xk, dx, x, y;
+
+    cout << "R = ";
+
+    cin >> R;
+
+    cout << endl;
+
+    cout << "XP = ";
+
+    cin >> xp;
+
+    cout << endl;
+
+    cout << "XK = ";
+
+    cin >> xk;
+
+    cout << endl;
+
+    cout << "DX = ";
+
+    cin >> dx;
+
+    cout << endl;
+
+    x = xp;
+
+    cout << fixed;
+
+    cout << "---------------------------" << endl;
+
+    cout << "|" << setw(5) << "x" << "   |"
+
+        << setw(7) << "Y" << "    |" << endl;
+
+    cout << "---------------------------" << endl;
+
+    while (x <= xk) {
+
+        if (x < (-1 - R))
+            y = -x - 2;
+        else
+            if ((x > (-1 - R)) && x <= -1)
+                y = sqrt(pow(R, 2) - pow((x + 1), 2));
+            else
+                if ((x > -1) && x <= 1)
+                    y = R;
+                else
+                    if ((x > 1) && x <= 2)
+                        y = R + (-1 - R) * (x - 1);
+                    else
+                        y = -1;
+
+        cout << "|" << setw(7) << setprecision(2) << x
+
+            << " |" << setw(10) << setprecision(3) << y
+
+            << " |" << endl;
+
+        x += dx;
+    }
+
+
+    system("pause");
+    return 0;
+
+
+}
